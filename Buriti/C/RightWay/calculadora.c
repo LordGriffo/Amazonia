e2b58c8c2e7638887be9422fbd5c98a7fb347acd
Lord Griffo <120344319+LordGriffo@users.noreply.github.com>
2023-07-06 23:41:43 +0000

    Add files via upload

---
+++ Buriti/C/RightWay/calculadora.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "calculadora.h"
+
+void realizarSoma(int num1, int num2) 
+{
+   return num1 + num2;
+}
+
+void realizarSubtracao(int num1, int num2) 
+{
+	return num1 - num2;
+}
+
+void realizarMultiplicacao(int num1, int num2) 
+{
+	return num1 * num2;
+}
+
+void realizarDivisao(int num1, int num2) 
+{
+	return num1 / num2;
+}
+
+void realizarResto(int num1, int num2) 
+{
+	return num1 % num2;
+}
+
+
