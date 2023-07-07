a6fcc0f1496cf666a54b45f675e06aa4dceb4ca1
Lord Griffo <120344319+LordGriffo@users.noreply.github.com>
2023-07-07 06:27:26 +0000

    Add files via upload

---
+++ Buriti/Cpp/RightWay/calculadora.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "calculadora.hpp"
+
+using namespace std;
+
+int realizarSoma(int num1, int num2) {
+    return num1 + num2;
+}
+
+int realizarSubtracao(int num1, int num2) {
+    return num1 - num2;
+}
+
+int realizarMultiplicacao(int num1, int num2) {
+    return num1 * num2;
+}
+
+int realizarDivisao(int num1, int num2) {
+    return num1 / num2;
+}
+
+int realizarResto(int num1, int num2) {
+    return num1 % num2;
+}
